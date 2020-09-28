44857032868ac358824bccb1f7b1b0be03a73226
Osman Malik <osman.malik.87@gmail.com>
2020-09-28 17:59:22 -0600

    Add support to tr_als_sampled for decomposing tensor stored in mat file on disk

--- tr_als_sampled.m
+++ tr_als_sampled.m
@@ -1,6 +1,9 @@
 function cores = tr_als_sampled(X, ranks, embedding_dims, varargin)
 %tr_als_sampled Compute tensor ring decomposition via sampled ALS
 %
+%For loading from file: It is assumed that the tensor is stored in a
+%variable Y in the mat file.
+%
 %cores = tr_als(X, ranks, embedding_dims) computes a tensor ring (TR) 
 %decomposition of the input N-dimensional array X by sampling the LS
 %problems using sketch sizes for each dimension given in embedding_dims.
@@ -26,25 +29,58 @@ function cores = tr_als_sampled(X, ranks, embedding_dims, varargin)
 addpath('help_functions\mtimesx\mtimesx_20110223')
 mtimesx('SPEED');
 
-%% Handle optional inputs
+%% Handle inputs 
 
+% Optional inputs
 params = inputParser;
 addParameter(params, 'tol', 1e-3, @isscalar);
 addParameter(params, 'maxiters', 50, @(x) isscalar(x) & x > 0);
 addParameter(params, 'resample', true, @isscalar);
 addParameter(params, 'verbose', false, @isscalar);
+addParameter(params, 'no_mat_inc', false);
 parse(params, varargin{:});
 
 tol = params.Results.tol;
 maxiters = params.Results.maxiters;
 resample = params.Results.resample;
 verbose = params.Results.verbose;
+no_mat_inc = params.Results.no_mat_inc;
+
+
+% Check if X is path to mat file on disk
+%   X_mat_flag is a flag that keeps track of if X is an array or path to
+%   a mat file on disk. In the latter case, X_mat will be a matfile that
+%   can be used to access elements of the mat file.
+if isa(X, 'char') || isa(X, 'string')
+    X_mat_flag = true;
+    X_mat = matfile(X, 'Writable', false);
+else
+    X_mat_flag = false;
+end
 
 %% Initialize cores, sampling probabilities and sampled cores
 
-sz = size(X);
+if X_mat_flag
+    sz = size(X_mat, 'Y');
+    N = length(sz);
+    col_cell = cell(1,N);
+    for n = 1:N
+        col_cell{n} = ':';
+    end
+    
+    % If value for no_mat_inc is provided, make sure it is a properly shaped
+    % vector.
+    if no_mat_inc(1)
+        if ~(size(no_mat_inc,1)==1 && size(no_mat_inc,2)==N)
+            no_mat_inc = no_mat_inc(1)*ones(1,N);
+        end
+    end
+else
+    sz = size(X);
+    N = length(sz);
+end
 cores = initialize_cores(sz, ranks);
-N = length(sz);
+
 sampling_probs = cell(1, N);
 for n = 2:N
     U = col(classical_mode_unfolding(cores{n}, 2));
@@ -117,14 +153,38 @@ for it = 1:maxiters
         G_sketch = rescaling .* G_sketch;
         
         % Construct sketched right hand side
-        %sz_shifted = [1 sz(1:end-1)];
-        %idx_prod = cumprod(sz_shifted);
-        samples_lin_idx_1 = 1 + (samples(:, idx)-1) * idx_prod(idx).';
-        %samples_lin_idx_2 = prod(sz_shifted(1:n))*(0:sz(n)-1).';
+        if X_mat_flag
+            % doing trick below since matfile arrays don't take linear
+            % indexes
+            slice_arg = col_cell;
+            inc_pts = linspace(0, sz(n), no_mat_inc(n)+1);
+            Xn_sketch = zeros(J, sz(n));
+            for m = 1:no_mat_inc(n)
+                
+                
+                slice_arg{n} = inc_pts(m)+1:inc_pts(m+1);
+                df = inc_pts(m+1)-inc_pts(m);
+                sz_slice = sz;
+                sz_slice(n) = df;
+                sz_slice_shifted = [1 sz_slice(1:end-1)];
+                idx_slice_prod = cumprod(sz_slice_shifted);
+
+                %samples_lin_idx = sub2ind(sz([idx n]), [repmat(samples(:, idx), df, 1) repelem((1:df).', J, 1)]);
+                samples_lin_idx_1 = 1 + (samples(:, idx)-1) * idx_slice_prod(idx).';
+                samples_lin_idx_2 = idx_slice_prod(n)*(0:df-1).';
+                samples_lin_idx = repmat(samples_lin_idx_1, df, 1) + repelem(samples_lin_idx_2, J, 1);
+                X_slice = X_mat.Y(slice_arg{:});
+                Xn_sketch(:, inc_pts(m)+1:inc_pts(m+1)) = reshape(X_slice(samples_lin_idx), J, df);
+            end
+        else
+            % X array in RAM -- use linear indexing
+            samples_lin_idx_1 = 1 + (samples(:, idx)-1) * idx_prod(idx).';
+            samples_lin_idx = repmat(samples_lin_idx_1, sz(n), 1) + slow_idx{n};
+            X_sampled = X(samples_lin_idx);
+            Xn_sketch = reshape(X_sampled, J, sz(n));
+        end
         
-        samples_lin_idx = repmat(samples_lin_idx_1, sz(n), 1) + slow_idx{n};
-        X_sampled = X(samples_lin_idx);
-        Xn_sketch = reshape(X_sampled, J, sz(n));
+        
         %Xn_sketch = reshape(X_sampled, sz(n), J);
         %Xn_sketch = permute(Xn_sketch, [2 1]);
         Xn_sketch = rescaling .* Xn_sketch;
@@ -154,7 +214,13 @@ for it = 1:maxiters
         Y = cores_2_tensor(cores);
 
         % Compute current relative error
-        er = norm(X(:)-Y(:))/norm(X(:));
+        if X_mat_flag
+            XX = X_mat.Y;
+            er = norm(XX(:)-Y(:))/norm(XX(:));
+        else
+            er = norm(X(:)-Y(:))/norm(X(:));
+        end
+        
         if verbose
             fprintf('\tRelative error after iteration %d: %.8f\n', it, er);
         end
