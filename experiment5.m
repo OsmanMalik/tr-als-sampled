40b8c7a2e8aedc5a1c4f5dba092d2ddb8af9610b
Osman Malik <osman.malik.87@gmail.com>
2021-01-29 07:11:47 -0700

    Add script for running classification experiment

---
+++ experiment5.m
@@ -0,0 +1,23 @@
+% The purpose of this script is to demonstrate how the TR decomposition can
+% be used for rapid feature extraction in a simple classification task
+
+path = "D:\data_sets\images\coil-100-downsampled\compressed_coil_100.mat";
+load(path)
+X = img_array;
+J = 2000;
+ranks = [5 5 5 5];
+tol = 1e-4;
+maxiters = 20;
+sz = size(X);
+exp5_tic = tic;
+cores = tr_als_sampled(X, ranks, J*ones(size(sz)), 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'resample', true, 'verbose', true);
+exp5_toc = toc(exp5_tic);
+
+feat_mat = reshape(permute(cores{4}, [2 1 3]), 7200, 25);
+Mdl = fitcknn(feat_mat, class_array, 'numneighbors', 1);
+cvmodel = crossval(Mdl);
+loss = kfoldLoss(cvmodel, 'lossfun', 'classiferror');
+accuracy = 1 - loss;
+fprintf('Accuracy is %.4f\n', accuracy);
+fprintf('Decomposition time was %.4f\n', exp5_toc);
+save('experiment5_results.mat', 'accuracy', 'exp5_toc');
