005aad5bba1f95a31e057ea585d61debbfc39d45
Osman Malik <osman.malik.87@gmail.com>
2021-03-24 16:23:53 -0600

    Add possibility to run experiment5 with other methods

--- experiment5.m
+++ experiment5.m
@@ -1,16 +1,31 @@
 % The purpose of this script is to demonstrate how the TR decomposition can
 % be used for rapid feature extraction in a simple classification task
 
+method = "tr_als";
+
 path = "D:\data_sets\images\coil-100-downsampled\compressed_coil_100.mat";
 load(path)
 X = img_array;
-J = 2000;
 ranks = [5 5 5 5];
 tol = 1e-4;
 maxiters = 20;
 sz = size(X);
 exp5_tic = tic;
-cores = tr_als_sampled(X, ranks, J*ones(size(sz)), 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'resample', true, 'verbose', true);
+switch method
+    case "tr_als"
+        cores = tr_als(X, ranks, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
+    case "rtr_als"
+        K = [20 20 3 200];
+        cores = rtr_als(X, ranks, K, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
+    case "tr_als_sampled"
+        J = 2000;
+        cores = tr_als_sampled(X, ranks, J*ones(size(sz)), 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'resample', true, 'verbose', true);
+    case "tr_svd"
+        cores = TRdecomp_ranks(X, ranks);
+    case "tr_svd_rand"
+        oversamp = 10;
+        cores = tr_svd_rand(X, ranks, oversamp);
+end
 exp5_toc = toc(exp5_tic);
 
 feat_mat = reshape(permute(cores{4}, [2 1 3]), 7200, 25);
