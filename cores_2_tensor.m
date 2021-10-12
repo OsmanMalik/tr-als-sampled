6ec95850ce21a2c79b2a7abece6bf9e920443dbc
Osman Asif Malik <osman.malik.87@gmail.com>
2021-10-12 14:28:22 -0600

    Add option to permute modes for improved speed in cores_2_tensor.m
    The new option makes it possible to permute the tensor so that the core tensor with the largest mode is incorporated last into the tensor in an effort to speed things up. Default behavior is to not use this permutation, i.e., the same as before.

--- cores_2_tensor.m
+++ cores_2_tensor.m
@@ -1,4 +1,4 @@
-function X = cores_2_tensor(cores)
+function X = cores_2_tensor(cores, varargin)
 %cores_2_tensor Convertes TR tensor given by cores to full dense tensor
 %
 %X = cores_2_tensor(cores) takes a cell array containing TR cores and
@@ -6,17 +6,33 @@ function X = cores_2_tensor(cores)
 %3-way standard Matlab arrays. The output will also be a standard Matlab
 %array.
 
+% Optional parameters
+params = inputParser;
+addParameter(params, 'permute_for_speed', false);
+parse(params, varargin{:});
+permute_for_speed = params.Results.permute_for_speed;
+
 N = length(cores);
-sz = zeros(1,N);
-for n = 1:N
-    sz(n) = size(cores{n},2);
+sz = cellfun(@(x) size(x,2), cores);
+
+if permute_for_speed
+    [~, max_idx] = max(sz);
+    perm_vec = mod((max_idx+1 : max_idx+N) - 1, N) + 1;
+    inv_perm_vec(perm_vec) = 1:N;
+    cores = cores(perm_vec);
+    sz = sz(perm_vec);
 end
+
 if isa(cores{1}, 'double') 
     G = subchain_matrix(cores, N);
     X = G * classical_mode_unfolding(cores{N}, 2).';
-    X = reshape(X, sz);
+    X = reshape(X, sz(:)');
 elseif isa(cores{1}, 'sptensor')
     
+end
+
+if permute_for_speed
+    X = permute(X, inv_perm_vec);
 end
 
 end
