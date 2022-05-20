f0d57e591b4a396d7c07e7e281a153dbc22eab9b
Osman Asif Malik <osman.malik.87@gmail.com>
2022-05-20 16:39:58 -0600

    Fix bug that comes up for sparse tensors
    numel(X) just returns 1 when applied to sparse tensors, so we need to use prod(size(X)) instead.

--- classical_mode_unfolding.m
+++ classical_mode_unfolding.m
@@ -5,7 +5,7 @@ N = max(ndims(X), n);
 szn = size(X,n);
 perm_vec = [n 1:n-1 n+1:N];
 X_perm = permute(X, perm_vec);
-Xn = reshape(X_perm, [szn, numel(X)/szn]);
+Xn = reshape(X_perm, [szn, prod(size(X))/szn]);
 
 if isa(Xn, 'sptensor')
     Xn = sptensor_mat_2_sparse(Xn);
